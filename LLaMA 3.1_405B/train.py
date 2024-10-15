88de1c50df19a6de69e0360d66a49b6f3daaa91a
Arjomandi <qy44lyfe@fauad.fau.de>
2024-10-15 14:08:22 +0200

    manual edit #4: Visuallize Predictions

--- LLaMA 3.1_405B/train.py
+++ LLaMA 3.1_405B/train.py
@@ -91,29 +91,50 @@ def visualize_losses(train_losses, val_losses, save_path):
     plt.savefig(os.path.join(save_path, 'losses.png'))
     plt.close()
 
-def visualize_predictions(model, device, loader, save_path):
+# def visualize_predictions(model, device, loader, save_path):
+#     model.eval()
+#     with torch.no_grad():
+#         images, masks = next(iter(loader))
+#         images, masks = images.to(device), masks.to(device)
+#         outputs = model(images)
+#         # Resize masks to match output size
+#         masks = F.interpolate(masks, size=outputs.shape[2:], mode='nearest')
+#         outputs = (outputs > 0.5).float()
+#         # Save predictions as images
+#         for i in range(images.shape[0]):
+#             image = images[i].cpu().numpy().squeeze()
+#             mask = masks[i].cpu().numpy().squeeze()
+#             output = outputs[i].cpu().numpy().squeeze()
+#             plt.figure(figsize=(12, 4))
+#             plt.subplot(1, 3, 1)
+#             plt.imshow(image, cmap='gray')
+#             plt.title('Image')
+#             plt.subplot(1, 3, 2)
+#             plt.imshow(mask, cmap='gray')
+#             plt.title('Mask')
+#             plt.subplot(1, 3, 3)
+#             plt.imshow(output, cmap='gray')
+#             plt.title('Prediction')
+#             plt.savefig(os.path.join(save_path, f'prediction_{i}.png'))
+#             plt.close()
+
+def visualize_predictions(model, dataloader, device, save_path):
     model.eval()
+    fig, axs = plt.subplots(5, 3, figsize=(12, 15))
+    axs = axs.ravel()
+
     with torch.no_grad():
-        images, masks = next(iter(loader))
-        images, masks = images.to(device), masks.to(device)
-        outputs = model(images)
-        # Resize masks to match output size
-        masks = F.interpolate(masks, size=outputs.shape[2:], mode='nearest')
-        outputs = (outputs > 0.5).float()
-        # Save predictions as images
-        for i in range(images.shape[0]):
-            image = images[i].cpu().numpy().squeeze()
-            mask = masks[i].cpu().numpy().squeeze()
-            output = outputs[i].cpu().numpy().squeeze()
-            plt.figure(figsize=(12, 4))
-            plt.subplot(1, 3, 1)
-            plt.imshow(image, cmap='gray')
-            plt.title('Image')
-            plt.subplot(1, 3, 2)
-            plt.imshow(mask, cmap='gray')
-            plt.title('Mask')
-            plt.subplot(1, 3, 3)
-            plt.imshow(output, cmap='gray')
-            plt.title('Prediction')
-            plt.savefig(os.path.join(save_path, f'prediction_{i}.png'))
-            plt.close()
+        for i, (images, masks) in enumerate(dataloader):
+            if i == 5:
+                break
+            images, masks = images.to(device), masks.to(device)
+            outputs = model(images)
+            # Resize masks to match output size
+            masks = F.interpolate(masks, size=outputs.shape[2:], mode='nearest')
+            outputs = (outputs > 0.5).float()
+            axs[i * 3].imshow(images[0].cpu().numpy().squeeze(), cmap='gray')
+            axs[i * 3 + 1].imshow(masks[0].cpu().numpy().squeeze(), cmap='gray')
+            axs[i * 3 + 2].imshow(outputs[0].cpu().numpy().squeeze(), cmap='gray')
+
+    plt.savefig(f'{save_path}/predictions.png')
+    plt.close()
